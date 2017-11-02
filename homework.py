0e109196aee090e3ffd77206c753d75a847e13d4
Andrew Thomas <andr3w.l.thomas@gmail.com>
2017-11-02 15:49:25 -0500

    added helper method to get mark up rate.

--- homework.py
+++ homework.py
@@ -7,3 +7,12 @@ def calculate_marked_up_value(base_price, num_people, item_type):
             item_type -- type of item
     """
     pass
+
+def __get_mark_up_rate(item_type):
+    """ Accepts item type and returns associated mark up rate. """
+    rates = {
+        'pharmaceutical': 0.075,
+        'food': 0.13,
+        'electronic': 0.02
+    }
+    return rates.get(item_type, 0)
