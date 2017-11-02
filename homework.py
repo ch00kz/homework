4af5b3b80b26f3e7be1b138e0facdca2c72274b7
Andrew Thomas <andr3w.l.thomas@gmail.com>
2017-11-02 17:29:34 -0500

    added simple validation of supplied input

--- homework.py
+++ homework.py
@@ -6,6 +6,9 @@ def calculate_marked_up_value(base_price, num_people, item_type):
             num_people -- number of people who have to do work on item
             item_type -- type of item
     """
+    __validate_arguments(base_price=base_price, num_people=num_people, 
+                         item_type=item_type)
+    
     # constant rates
     FLAT_MARK_UP_RATE = 0.05
     PER_PERSON_MARK_UP_RATE = 0.012
@@ -37,3 +40,28 @@ def __get_type_mark_up_rate(item_type):
         'electronic': 0.02
     }
     return rates.get(item_type, 0)
+
+
+def __validate_arguments(**kwargs):
+    """ Provides simple validation of supplied arguments.
+    """
+    base_price = kwargs.get('base_price')
+    valid_base_price = isinstance(base_price, (int, float)) and base_price >= 0
+    if not valid_base_price:
+        raise ValueError(
+            "Supplied base_price must be a non-negative integer or float."
+            )
+
+    num_people = kwargs.get('num_people')
+    valid_num_people = isinstance(num_people, int) and num_people >= 0
+    if not valid_num_people:
+        raise ValueError(
+            "Supplied num_people must be a non-negative integer."
+            )
+
+    item_type = kwargs.get('item_type')
+    valid_item_type = isinstance(item_type, str) and len(item_type) > 0
+    if not valid_item_type:
+        raise ValueError(
+            "Supplied item_type must be a string of length > 1"
+            )
