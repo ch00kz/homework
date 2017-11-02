3dd98880bf719996232d84df7dfccd0d77553292
Andrew Thomas <andr3w.l.thomas@gmail.com>
2017-11-02 15:44:29 -0500

    added method stub for calculate_marked_up_value

---
+++ homework.py
@@ -0,0 +1,9 @@
+def calculate_marked_up_value(base_price, num_people, item_type):
+    """ Accepts item information and returns marked up price.
+
+        Arguments:
+            base_price -- base price of item
+            num_people -- number of people who have to do work on item
+            item_type -- type of item
+    """
+    pass
